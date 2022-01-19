e8ea9cfc6201d3eefd1a13a268d6cf2a349b9ef2
Weuller Oliveira <weullerwww@gmail.com>
2022-01-19 09:14:09 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Partida de xadrez");
-
+		Position p = new Position(2, 5);
+		System.out.println(p);
 	}
 
 }
